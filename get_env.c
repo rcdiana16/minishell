a54f12a8dd34ec75ef186a1d92cc9cbd74c8aa2c
cosmos510 <maximemartin510@gmail.com>
2025-02-25 19:08:57 +0100

    update get_env funtion

--- get_env.c
+++ get_env.c
@@ -6,26 +6,31 @@
 /*   By: cosmos <cosmos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 13:58:26 by cosmos            #+#    #+#             */
-/*   Updated: 2025/02/25 14:13:07 by cosmos           ###   ########.fr       */
+/*   Updated: 2025/02/25 19:07:09 by cosmos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	**get_env(char **env)
+char	*get_env(char *var, char **env)
 {
-	char	**env_dup;
 	int		i;
+	int		var_len;
+	char	*value_start;
 
 	i = 0;
-	while (env[i])
-		i++;
-	env_dup = malloc(i * (sizeof(char *)));
-	i = 0;
+	var_len = ft_strlen(var);
+	if (env == NULL || *env == NULL || var == NULL)
+		return NULL;
+
 	while (env[i])
 	{
-		env_dup[i] = ft_strdup(env[i]);
+		if (ft_strncmp(env[i], var, var_len) == 0)
+		{
+			value_start = &env[i][var_len + 1];
+			return (value_start);
+		}
 		i++;
 	}
-	return (env_dup);
+	return (NULL);
 }
