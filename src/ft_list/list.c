bf8a7a50de79d7c3b883d6b616405529be8f17ba
cosmos510 <maximemartin510@gmail.com>
2025-02-28 21:22:04 +0100

    add list for env

---
+++ src/ft_list/list.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cosmos <cosmos@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 21:08:20 by cosmos            #+#    #+#             */
+/*   Updated: 2025/02/28 21:20:38 by cosmos           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+t_env	*create_node(char *var, char *val)
+{
+	t_env	*new_node;
+
+	new_node = malloc(sizeof(t_env));
+	if (!new_node)
+		return (NULL);
+	new_node->variable = strdup(var);
+	new_node->value = strdup(val);
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	append_node(t_env **head, char *var, char *val)
+{
+	t_env	*new_node;
+	t_env	*temp;
+
+	new_node = create_node(var, val);
+	if (!new_node)
+		return ;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return ;
+	}
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
+}
+
+void	get_list_env(char **envp)
+{
+	t_env	*env_list;
+	int		i;
+	char	*equal_sign;
+
+	env_list = NULL;
+	i = 0;
+	while (envp[i])
+	{
+		equal_sign = strchr(envp[i], '=');
+		if (equal_sign)
+		{
+			*equal_sign = '\0';
+			append_node(&env_list, envp[i], equal_sign + 1);
+			*equal_sign = '=';
+		}
+		i++;
+	}
+}
